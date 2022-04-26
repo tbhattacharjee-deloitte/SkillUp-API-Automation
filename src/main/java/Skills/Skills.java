1c8840e95117710f3c412e6fe8dcf9bd21820d8a
tbhattacharjee-deloitte <tbhattacharjee-deloitte@users.noreply.github.com>
2022-04-26 23:29:31 +0530

    creating a new skill and deleting the same skill

--- src/main/java/Skills/Skills.java
+++ src/main/java/Skills/Skills.java
@@ -17,12 +17,43 @@ import static io.restassured.RestAssured.given;
 
 public class Skills {
     private final String token;
+    private final Logger logger;
+    private final ResponseSpecification resSpec;
+    private final RequestSpecification reqSpec;
+    private int createdSkillId;
 
-    public Skills(String token) {
+    public Skills(String token, Logger logger) {
         this.token = token;
+        this.logger = logger;
+
+        // building reqSpec
+        RequestSpecBuilder reqBuilder = new RequestSpecBuilder();
+        reqBuilder.setBaseUri(BaseProp.baseUrl)
+                .addHeader("Content-Type", "application/json")
+                .addHeader("Authorization", "Bearer " + token);
+        reqSpec = RestAssured.with().spec(reqBuilder.build());
+
+        // building resSpec
+        ResponseSpecBuilder resBuilder = new ResponseSpecBuilder();
+        resBuilder.expectContentType(ContentType.JSON);
+        resSpec = resBuilder.build();
     }
 
     public void createSkill(String skillName) {
+        JSONObject obj = new JSONObject();
+        obj.put("skillName", skillName);
+        Response response = given().spec(reqSpec).body(obj.toString())
+                .post("/api/skills/").then().spec(resSpec).extract().response();
+        assert response.statusCode() == BaseProp.OK;
+        JSONObject res = new JSONObject(response.asString());
+        logger.debug(res.toString());
+        this.createdSkillId = Integer.parseInt(res.get("skillId").toString());
+    }
 
+    public void deleteLastCreatedSkill() {
+        Response response = given().spec(reqSpec)
+                .delete("/api/skills/" + this.createdSkillId).then()
+                .extract().response();
+        assert response.statusCode() == BaseProp.OK;
     }
 }
--- src/test/java/TestClassPackage/SkillsApiTest.java
+++ src/test/java/TestClassPackage/SkillsApiTest.java
@@ -2,12 +2,14 @@ package TestClassPackage;
 
 import Auth.Auth;
 import Base.BaseClass;
+import Skills.Skills;
 import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class SkillsApiTest extends BaseClass {
     private String loginToken;
+    private Skills skills;
 
     public SkillsApiTest() {
         super();
@@ -17,11 +19,17 @@ public class SkillsApiTest extends BaseClass {
     @BeforeClass
     public void login() {
         loginToken = Auth.login("vivek", "vivek123");
+        skills = new Skills(loginToken, logger);
         logger.debug(loginToken);
     }
 
     @Test (priority = 1)
     public void createSkill() {
+        skills.createSkill("AI");
+    }
 
+    @Test (priority = 2)
+    public void deleteLastSkill() {
+        skills.deleteLastCreatedSkill();
     }
 }
