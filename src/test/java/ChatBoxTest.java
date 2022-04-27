0ddcc5d14b47c180411e88d56f59b66a676d36a3
sweetasingh-deloitte <101088102+sweetasingh-deloitte@users.noreply.github.com>
2022-04-28 01:23:51 +0530

    created test class for chatbox module

---
+++ src/test/java/ChatBoxTest.java
@@ -0,0 +1,54 @@
+import Auth.Auth;
+import Base.BaseProp;
+import chatbox.ChatBox;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+import org.apache.logging.log4j.Logger;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class ChatBoxTest {
+    private String loginToken;
+    private ChatBox chatBox;
+
+
+    public ChatBoxTest() {
+        super();
+    }
+
+    @BeforeClass
+    public void login() {
+        loginToken = Auth.login("vivek", "vivek123");
+        chatBox = new ChatBox(loginToken);
+        System.out.println(loginToken);
+    }
+
+    @Test(priority = 1)
+    public void createChatBox() {
+        chatBox.createChatBox();
+    }
+
+    @Test (priority = 2)
+    public void getAllChatBox() {
+        chatBox.getAllChatBox();
+    }
+
+    @Test (priority = 3)
+    public void getAllChatOfChatbox() {
+        chatBox.getAllChatOfChatbox();
+    }
+
+    @Test(priority = 4)
+    public void addChatToChatbox() {
+        chatBox.addChatToChatbox(12);
+    }
+
+
+
+
+}
